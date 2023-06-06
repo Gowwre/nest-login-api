ec1597583b5a0db77b444fadb07a2107748cada8
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-06-06 17:28:16 +0700

    feat: implemented passport-jwt auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,11 +10,18 @@ import { Module } from '@nestjs/common';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {
-}
+export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,8 @@ import { UserService } from 'src/user/user.service';
 @Injectable()
 export class AuthService {
   constructor(
-    private userService: UserService   
+    private userService: UserService,
+    private jwtService: JwtService,
   ) {}
 
   async validateUser(email: string, password: string) {
@@ -24,5 +25,11 @@ export class AuthService {
       console.log(error);
     }
   }
-  
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
